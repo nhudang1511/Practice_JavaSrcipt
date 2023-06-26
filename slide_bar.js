6f9431ff07f902d83a4d06d9527785bdc51f9765
nhu_nguyen <nhudang1511@gmail.com>
2023-06-26 08:04:11 +0700

    bai4

--- slide_bar.js
+++ slide_bar.js
@@ -3,9 +3,9 @@ const btn_close = document.querySelector(".close-btn");
 const slide_bar_toggle=document.querySelector(".sidebar-toggle");
 
 slide_bar_toggle.addEventListener("click",function(){
-    slide_bar.classList.toggle("open-slide")
+    slide_bar.classList.toggle("open-slide");
 });
 
 btn_close.addEventListener("click",function(){
-    slide_bar.classList.add("open-slide");
+    slide_bar.classList.remove("open-slide");
 });
