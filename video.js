c471f34f91739d2cfaef8202ba60e6bbb2ae073d
nhu_nguyen <nhudang1511@gmail.com>
2023-06-29 09:44:03 +0700

    bai9

--- video.js
+++ video.js
@@ -1,5 +1,11 @@
 const switch_btn = document.querySelector(".switch-button");
 const video = document.querySelector(".video-container");
+const preload = document.querySelector(".preload");
+
+window.addEventListener("load",function(){
+    preload.classList.add("hide");
+});
+
 switch_btn.addEventListener("click",function(){
     if(!switch_btn.classList.contains("new-switch")){
         switch_btn.classList.add("new-switch");
